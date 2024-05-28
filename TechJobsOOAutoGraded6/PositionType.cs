726ce5f15ba77b9d63f6d833774badc451d255bd
Matthew Creek <matthew.creek27@gmail.com>
2024-05-28 16:17:42 -0500

    task 2 complete

--- TechJobsOOAutoGraded6/PositionType.cs
+++ TechJobsOOAutoGraded6/PositionType.cs
@@ -18,7 +18,21 @@ namespace TechJobsOOAutoGraded6
             Value = value;
         }
 
-        // TODO: Task 2: Add custom Equals(), GetHashCode(), and ToString() methods.
+        public override bool Equals(object? obj)
+        {
+            return obj is PositionType type &&
+                   Id == type.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id);
+        }
+
+        public override string ToString()
+        {
+            return Value;
+        }
     }
 }
 
